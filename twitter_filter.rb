5d64b12cc7129fda28a03d4ab9537492d0588110
andistuder <andi@cenatus.org>
2012-06-03 01:24:50 +0100

    included twitter authentication instructions

--- twitter_filter.rb
+++ twitter_filter.rb
@@ -1,6 +1,20 @@
 require 'tweetstream'
 require File.join(File.dirname(__FILE__), 'tweet_store')
+
 load File.join(File.dirname(__FILE__), 'twitter_authentication.rb')
+# if Twitter authentication file not available uncomment and update config below
+# TweetStream.configure do |config|
+#   config.username             = "USERNAME"
+#   config.password             = "PASSWORD"
+#   config.auth_method          = :basic
+#   # OAuth preferred
+#   # config.consumer_key       = '1234567890'
+#   # config.consumer_secret    = 'asdfghjkl'
+#   # config.oauth_token        = 'asdfghjkl'
+#   # config.oauth_token_secret = 'asdfghjkl'
+#   # config.auth_method        = :oauth
+# end
+
 
 STORE = TweetStore.new
 
